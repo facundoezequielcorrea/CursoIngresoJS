38f12baf1c0f8a3c84d39eb91ec12b3cbfa20b0a
facundoezequielcorrea <36193972+facundoezequielcorrea@users.noreply.github.com>
2018-02-06 10:16:45 -0300

    Ejercicios 1 y 2
    Var, alert, comentar, concatenar y prompt

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,14 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre=prompt ("Ingrese nombre", "Nombre completo");
+	alert ("Su nombre es: "+nombre)
+	/*comentario
+	alert (nombre);
+	alert ("nombre");
+	nombre= ("Facundo Ezequiel Correa")
+	alert (nombre);
+	*/
+	//var person = prompt("Please enter your name", "Harry Potter");
 }
 
