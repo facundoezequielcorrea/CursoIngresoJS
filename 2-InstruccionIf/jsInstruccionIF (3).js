e8c736e4f1ea2a7897a237de12cdb56c037f2fb8
facundoezequielcorrea <36193972+facundoezequielcorrea@users.noreply.github.com>
2018-02-07 12:25:45 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
 
+	edad=document.getElementById('edad').value;
+
+	if (edad>17)
+	{	
+		alert ("Eres mayor de edad")
+	}
+	if (edad<=17)
+	{
+		alert ("Eres menor de edad")
+	}	
+	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,33 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+
+	edad=document.getElementById('edad').value;
+
+	if (edad<12)
+	{
+		alert ("eres menor")
+	}
+	else
+	{
 
 
+		if (edad>17)
+		{
+			alert("eres mayor") 
+		}
+		else
+		{
+				alert ("no Eres adolecente")
+			
+		}
+
+
+	}
+
+	
+			
 
 
 }//FIN DE LA FUNCIÓN
