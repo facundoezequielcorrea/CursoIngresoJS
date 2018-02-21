8bd3053802535aa976824f735e7d160db3912127
facundoezequielcorrea <36193972+facundoezequielcorrea@users.noreply.github.com>
2018-02-21 12:11:39 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,20 @@
 function Mostrar()
 {
+	var iteraciones=10;
 
+	for(contador=0;contador<iteraciones;contador++)
+	{
+		document.write ("<br>"+contador);
+		if (contador==10)
+		{
+			break;
+		}
+		if (contador==5)
+		{
+			continue;
+		}
 
+		document.write ("despues de continue");
+	}
+	document.write ("fin del for");
 }
