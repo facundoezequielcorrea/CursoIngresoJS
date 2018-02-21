b2f36e085680eb8f7d248983bcebb2e45c0716cb
facundoezequielcorrea <36193972+facundoezequielcorrea@users.noreply.github.com>
2018-02-21 10:46:47 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,46 @@
 function Mostrar()
 {
+	//var contador=0;
+	var iteraciones=10;
+	var contadordepar=0;
+	var contadordeimpar=0;
+	var contadordeceros=0;
+	var contadordepositivos=0;
+	var contadordenegativos=0;
 
+	for(contador=-10;contador<iteraciones;contador++)
+	{
+		document.write ("<br>"+contador);
+		if (contador==0)
+		{
+			contadordeceros++;
+		}
+		else
+		{	
+			if (contador<0)
+			{
+				contadordenegativos++;	
+			}
+			if (contador>0)
+			{
+				contadordepositivos++;
+			}
+		
+			if (contador%2==0)
+			{
+				contadordepar++;
+			}
+			else
+			{
+				contadordeimpar++;
+			}
+			
+		}	
+	}
+	document.write ("<br>"+contador);
+	document.write ("<br> par: "+contadordepar);
+	document.write ("<br> impar: "+contadordeimpar);
+	document.write ("<br> ceros: "+contadordeceros);
+	document.write ("<br> negativos: "+contadordenegativos)
+	document.write ("<br> negativos: "+contadordepositivos)
 }
