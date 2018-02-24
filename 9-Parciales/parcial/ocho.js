e7dabbc9cfafc8b56f5c711e0a9f7b8211019899
facundoezequielcorrea <36193972+facundoezequielcorrea@users.noreply.github.com>
2018-02-24 12:10:03 -0300

    Add files via upload

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,5 +1,15 @@
 /*ingreso de alumnos sin saber cantidad final pedir nombre 
-cuantos alumnos ingrese*/
+cuantos alumnos ingrese
+promedio todal
+cantidad de mujeres
+cantidad de hombres
+cantidad de varones desaprobados
+nombre de la mejor nota
+promedio de la mujeres (nota)
+sexo del menor edad
+promedio de las edades de los aprobados
+cantidad de notas pares
+camtidad de notas impares*/
 function Mostrar()
 {
 	var nombre;
@@ -10,6 +20,19 @@ function Mostrar()
 	var promedio;
 	var canthombre=0;
 	var cantmujer=0;
+	var hombresdesaprob=0;
+	var nombremejornota;
+	var mejornota=0;
+	var notamujer=0;
+	var promnotamujer;
+	var edad=0;
+	var menoredad=0;
+	var sexomenoredad;
+	var edadesaprobados=0;
+	var cantalumnosaprob=0;
+	var promaprobados;
+	var notapar=0;
+	var notaimpar=0;
 
 	while (ingresaalumno!="no")
 	{
@@ -17,6 +40,7 @@ function Mostrar()
 		contadordealumnos++;
 		nota= prompt ("por favor ingrese nota del alumno");
 		nota=parseInt(nota);
+		
 		while (nota<0 || nota>10)
 		{
 			nota= prompt ("nota incorrecta por favor ingrese nota del alumno nuevamente");
@@ -30,15 +54,59 @@ function Mostrar()
 		if (sexo=="f")
 		{
 			cantmujer++;
+			notamujer=notamujer+nota;
 		}else
 		{
 			canthombre++;
 		}
+		edad= prompt ("por favor ingrese la edad del alumno");
+		while (edad<0 || edad>100)
+		{
+			edad= prompt ("edad incorrecta por favor ingrese la edad del alumno nuevamente");
+		}
+		if (sexo=="m" && nota<4)
+		{
+			hombresdesaprob++;
+		}
+		if (nota>mejornota)
+		{
+			nota=mejornota;
+			nombremejornota=nombre;
+		}
+		if(menoredad>edad)
+		{
+			menoredad=edad;
+			sexomenoredad=sexo;
+		}
+		if(nota>3)
+		{
+			cantalumnosaprob++;
+			edadesaprobados=edadesaprobados+edad;
+		}
+		if (nota%2==0)
+		{
+			notapar++;
+		}else
+		{
+			notaimpar++;
+		}
+
 		acumnota=acumnota+nota;
 
 		ingresaalumno= prompt ("Ingrese NO para dejar de ingresar nombres");
 	}
 	promedio= acumnota/contadordealumnos;
+	promnotamujer=notamujer/cantmujer;
+	promaprobados=edadesaprobados/cantalumnosaprob;
 	document.write ("usted ingreso "+contadordealumnos+" alumnos");
-	document.write ("la nota promedio es "+promedio);
+	document.write ("<br> la nota promedio es "+promedio);
+	document.write ("<br> la cantidad de hombres es "+canthombre);
+	document.write ("<br> la cantidad de mujeres es "+cantmujer);
+	document.write ("<br> la cantidad de hombres desaprobados son "+hombresdesaprob);
+	document.write ("<br> el alumno con mejor nota es "+nombremejornota);
+	document.write ("<br> el promedio de nota de mujeres es "+promnotamujer);
+	document.write ("<br> el sexo del alumno con menor edad es "+sexomenoredad);
+	document.write ("<br> el promedio de edad de alumnos aprobados es "+promaprobados);
+	document.write ("<br> la cantidad de notas pares es "+notapar);
+	document.write ("<br> la cantidad de notas impares es "+notaimpar);
 }
